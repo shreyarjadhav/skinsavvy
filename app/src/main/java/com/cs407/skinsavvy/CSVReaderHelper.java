cc9c5fd1e02f6627db8ca13d0768bc8592410139
varshi310 <varshitha.daggolu@gmail.com>
2023-11-28 21:31:10 -0600

    Fixed the Dependency issues

--- app/src/main/java/com/cs407/skinsavvy/CSVReaderHelper.java
+++ app/src/main/java/com/cs407/skinsavvy/CSVReaderHelper.java
@@ -1,90 +1,90 @@
-package com.cs407.skinsavvy;
-
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-public class CSVReaderHelper {
-
-    public static void readCSVAndInsertIntoDatabase(Context context) {
-        SkincareDatabaseHelper databaseHelper = new SkincareDatabaseHelper(context);
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
-
-        try {
-            // Update the path to your CSV file
-            String csvFilePath = "C:\\Users\\Sofea\\Documents\\CS407\\Labs\\skinsavvy\\ingredients.csv";
-            CSVReader csvReader = new CSVReader(new FileReader(csvFilePath));
-            List<String[]> csvData = csvReader.readAll();
-
-            for (String[] row : csvData) {
-                ContentValues values = new ContentValues();
-                values.put(SkincareDatabaseHelper.COLUMN_NAME, row[0]);
-                values.put(SkincareDatabaseHelper.COLUMN_ACNE, Integer.parseInt(row[1]));
-                values.put(SkincareDatabaseHelper.COLUMN_OILY, Integer.parseInt(row[2]));
-                values.put(SkincareDatabaseHelper.COLUMN_DRY, Integer.parseInt(row[3]));
-                values.put(SkincareDatabaseHelper.COLUMN_COMBO, Integer.parseInt(row[4]));
-
-                db.insert(SkincareDatabaseHelper.TABLE_NAME, null, values);
-            }
-
-            // Log the data to the terminal
-            logDataFromDatabase(db);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.close();
-        }
-    }
-
-    private static void logDataFromDatabase(SQLiteDatabase db) {
-        // Define the columns you want to retrieve
-        String[] projection = {
-                SkincareDatabaseHelper.COLUMN_NAME,
-                SkincareDatabaseHelper.COLUMN_ACNE,
-                SkincareDatabaseHelper.COLUMN_OILY,
-                SkincareDatabaseHelper.COLUMN_DRY,
-                SkincareDatabaseHelper.COLUMN_COMBO
-        };
-
-        // Perform a query on the database
-        Cursor cursor = db.query(
-                SkincareDatabaseHelper.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                null
-        );
-
-        // Log the data to the terminal
-        while (cursor.moveToNext()) {
-            String name = cursor.getString(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_NAME));
-            int acne = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_ACNE));
-            int oily = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_OILY));
-            int dry = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_DRY));
-            int combo = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_COMBO));
-
-            // Log the data
-            Log.d("DatabaseOutput", "Name: " + name + ", Acne: " + acne + ", Oily: " + oily +
-                    ", Dry: " + dry + ", Combo: " + combo);
-        }
-
-        cursor.close();
-    }
-}
-
-
+//package com.cs407.skinsavvy;
+//
+//
+//import android.content.ContentValues;
+//import android.content.Context;
+//import android.database.Cursor;
+//import android.database.sqlite.SQLiteDatabase;
+//import android.util.Log;
+//
+//import com.opencsv.CSVReader;
+//import com.opencsv.exceptions.CsvException;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.List;
+//
+//public class CSVReaderHelper {
+//
+//    public static void readCSVAndInsertIntoDatabase(Context context) {
+//        SkincareDatabaseHelper databaseHelper = new SkincareDatabaseHelper(context);
+//        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+//
+//        try {
+//            // Update the path to your CSV file
+//            String csvFilePath = "C:\\Users\\Sofea\\Documents\\CS407\\Labs\\skinsavvy\\ingredients.csv";
+//            CSVReader csvReader = new CSVReader(new FileReader(csvFilePath));
+//            List<String[]> csvData = csvReader.readAll();
+//
+//            for (String[] row : csvData) {
+//                ContentValues values = new ContentValues();
+//                values.put(SkincareDatabaseHelper.COLUMN_NAME, row[0]);
+//                values.put(SkincareDatabaseHelper.COLUMN_ACNE, Integer.parseInt(row[1]));
+//                values.put(SkincareDatabaseHelper.COLUMN_OILY, Integer.parseInt(row[2]));
+//                values.put(SkincareDatabaseHelper.COLUMN_DRY, Integer.parseInt(row[3]));
+//                values.put(SkincareDatabaseHelper.COLUMN_COMBO, Integer.parseInt(row[4]));
+//
+//                db.insert(SkincareDatabaseHelper.TABLE_NAME, null, values);
+//            }
+//
+//            // Log the data to the terminal
+//            logDataFromDatabase(db);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (CsvException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            db.close();
+//        }
+//    }
+//
+//    private static void logDataFromDatabase(SQLiteDatabase db) {
+//        // Define the columns you want to retrieve
+//        String[] projection = {
+//                SkincareDatabaseHelper.COLUMN_NAME,
+//                SkincareDatabaseHelper.COLUMN_ACNE,
+//                SkincareDatabaseHelper.COLUMN_OILY,
+//                SkincareDatabaseHelper.COLUMN_DRY,
+//                SkincareDatabaseHelper.COLUMN_COMBO
+//        };
+//
+//        // Perform a query on the database
+//        Cursor cursor = db.query(
+//                SkincareDatabaseHelper.TABLE_NAME,
+//                projection,
+//                null,
+//                null,
+//                null,
+//                null,
+//                null
+//        );
+//
+//        // Log the data to the terminal
+//        while (cursor.moveToNext()) {
+//            String name = cursor.getString(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_NAME));
+//            int acne = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_ACNE));
+//            int oily = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_OILY));
+//            int dry = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_DRY));
+//            int combo = cursor.getInt(cursor.getColumnIndexOrThrow(SkincareDatabaseHelper.COLUMN_COMBO));
+//
+//            // Log the data
+//            Log.d("DatabaseOutput", "Name: " + name + ", Acne: " + acne + ", Oily: " + oily +
+//                    ", Dry: " + dry + ", Combo: " + combo);
+//        }
+//
+//        cursor.close();
+//    }
+//}
+//
+//
--- app/src/main/java/com/cs407/skinsavvy/MainActivity.java
+++ app/src/main/java/com/cs407/skinsavvy/MainActivity.java
@@ -2,32 +2,25 @@ package com.cs407.skinsavvy;
 
 import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.IntentSender;
+
 import android.os.Bundle;
-import android.os.PersistableBundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
