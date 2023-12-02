d5ddaea29157fde779924e4313fa63310b28058f
varshi310 <varshitha.daggolu@gmail.com>
2023-12-02 15:52:33 -0600

    linked survey page to home page, made xml for manual search

--- app/src/main/java/com/cs407/skinsavvy/MainActivity.java
+++ app/src/main/java/com/cs407/skinsavvy/MainActivity.java
@@ -2,19 +2,15 @@ package com.cs407.skinsavvy;
 
 import static android.content.ContentValues.TAG;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-
 import android.widget.Toast;
-
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -28,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     GoogleSignInClient gsc;
     ImageView googleBtn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,19 +36,20 @@ public class MainActivity extends AppCompatActivity {
                 .requestProfile()
                 .build();
 
-
-        gsc = GoogleSignIn.getClient(this,gso);
-        GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), gso.getScopeArray());
-
+        gsc = GoogleSignIn.getClient(this, gso);
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if(acct!=null){
-            navigateToHomePage();
+        if (acct != null) {
+            if (!hasVisitedSurveyPage(acct.getId())) {
+                navigateToSurveyPage(acct.getId());
+            } else {
+                navigateToHomePage();
+            }
         }
 
-        googleBtn.setOnClickListener(new View.OnClickListener(){
+        googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 signIn();
             }
         });
@@ -63,27 +59,43 @@ public class MainActivity extends AppCompatActivity {
         Intent signInIntent = gsc.getSignInIntent();
         startActivityForResult(signInIntent, 1000);
     }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == 1000) {
-                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                try {
-                    GoogleSignInAccount account = task.getResult(ApiException.class);
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (!hasVisitedSurveyPage(account.getId())) {
+                    navigateToSurveyPage(account.getId());
+                } else {
                     navigateToHomePage();
-                } catch (ApiException e) {
-                    Toast.makeText(getApplicationContext(), "Something Went Wrong ", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "Google sign-in failed with ApiException: " + e.getStatusCode(), e);
                 }
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something Went Wrong ", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Google sign-in failed with ApiException: " + e.getStatusCode(), e);
             }
         }
+    }
 
+    boolean hasVisitedSurveyPage(String accountId) {
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        return sharedPreferences.getBoolean("visitedSurveyPage_" + accountId, false);
+    }
 
-    void navigateToHomePage(){
+    void navigateToSurveyPage(String accountId) {
+        finish();
+        Intent intent = new Intent(MainActivity.this, SurveyActivity.class);
+        startActivity(intent);
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        sharedPreferences.edit().putBoolean("visitedSurveyPage_" + accountId, true).apply();
+    }
+
+    void navigateToHomePage() {
         finish();
         Intent intent = new Intent(MainActivity.this, HomePage.class);
         startActivity(intent);
     }
-
-
 }
