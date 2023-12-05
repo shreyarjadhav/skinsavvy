1bf6746af245463a1461f165df2e699c418dac47
varshi310 <varshitha.daggolu@gmail.com>
2023-12-05 13:06:42 -0600

    manual results

--- app/src/main/java/com/cs407/skinsavvy/ManualSearch.java
+++ app/src/main/java/com/cs407/skinsavvy/ManualSearch.java
@@ -2,7 +2,11 @@ package com.cs407.skinsavvy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ManualSearch extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class ManualSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_search);
+
+        EditText ingredientsList = findViewById(R.id.ingredientsList);
+
+        String[] values = ingredientsList.toString().split(",");
+
+        Button getResults = findViewById(R.id.getResults);
+
+
+
+
     }
+
+    public void navigateToResultsPage(View view){
+        Intent intent = new Intent(this, ManualResult.class);
+        startActivity(intent);
+    }
+
 }
