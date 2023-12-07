25d56e856d0c57d5bf797d49d4e5b4110f5da5d8
sfearahman <sabdrahman@wisc.edu>
2023-12-06 20:40:00 -0600

    Databases update (removed CSVReaderHelper.java, added SurveyDatabaseHelper.java)

--- app/src/main/java/com/cs407/skinsavvy/SurveyActivity.java
+++ app/src/main/java/com/cs407/skinsavvy/SurveyActivity.java
@@ -1,21 +1,38 @@
 package com.cs407.skinsavvy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.ColorStateList;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SurveyActivity extends AppCompatActivity {
     private Button oilyButton, dryButton, acneProneButton, comboButton;
     private EditText allergiesEditText;
-    private String selectedSkinType = "";
+    private boolean isOily = false;
+    private boolean isDry = false;
+    private boolean isAcne = false;
+    private boolean isCombo = false;
     private String allergiesData = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new LoadDataTask().execute(); // to load ingredients.csv into database
         setContentView(R.layout.activity_survey);
 
         // init UI elements
@@ -26,30 +43,175 @@ public class SurveyActivity extends AppCompatActivity {
         allergiesEditText = findViewById(R.id.allergies_edit_text);
 
         // set onClickListeners for skin type buttons
+        acneProneButton.setOnClickListener(view -> selectSkinType("acne"));
         oilyButton.setOnClickListener(view -> selectSkinType("oily"));
         dryButton.setOnClickListener(view -> selectSkinType("dry"));
-        acneProneButton.setOnClickListener(view -> selectSkinType("acne_prone"));
         comboButton.setOnClickListener(view -> selectSkinType("combo"));
 
         // handle form submission (submit button click)
         Button submitButton = findViewById(R.id.submit_button);
-        submitButton.setOnClickListener(view -> goToHomePage());
+        submitButton.setOnClickListener(view -> goToProfilePage());
     }
 
-    // method to handle skin type selection
+    // method to handle multiple skin type selection
     private void selectSkinType(String skinType) {
-        selectedSkinType = skinType;
+        // toggle the boolean value
+        switch (skinType) {
+            case "oily":
+                isOily = !isOily;
+                updateButtonColors();
+                break;
+
+            case "dry":
+                isDry = !isDry;
+                updateButtonColors();
+                break;
+
+            case "acne":
+                isAcne = !isAcne;
+                updateButtonColors();
+                break;
+
+            case "combo":
+                isCombo = !isCombo;
+                updateButtonColors();
+                break;
+
+            default:
+                break;
+        }
     }
 
-    // method to retrieve allergies and nav to Home Page
-    private void goToHomePage() {
+    // method to update button colors based on boolean variables
+    private void updateButtonColors() {
+        oilyButton.setBackgroundTintList(
+                isOily ? ColorStateList.valueOf(Color.parseColor("#FFFFFF")) : ColorStateList.valueOf(Color.parseColor("#6750A4")));
+        oilyButton.setTextColor(isOily ? Color.parseColor("#6750A4") : Color.parseColor("#FFFFFF"));
+
+        dryButton.setBackgroundTintList(
+                isDry ? ColorStateList.valueOf(Color.parseColor("#FFFFFF")) : ColorStateList.valueOf(Color.parseColor("#6750A4")));
+        dryButton.setTextColor(isDry ? Color.parseColor("#6750A4") : Color.parseColor("#FFFFFF"));
+
+        acneProneButton.setBackgroundTintList(
+                isAcne ? ColorStateList.valueOf(Color.parseColor("#FFFFFF")) : ColorStateList.valueOf(Color.parseColor("#6750A4")));
+        acneProneButton.setTextColor(isAcne ? Color.parseColor("#6750A4") : Color.parseColor("#FFFFFF"));
+
+        comboButton.setBackgroundTintList(
+                isCombo ? ColorStateList.valueOf(Color.parseColor("#FFFFFF")) : ColorStateList.valueOf(Color.parseColor("#6750A4")));
+        comboButton.setTextColor(isCombo ? Color.parseColor("#6750A4") : Color.parseColor("#FFFFFF"));
+    }
+
+    // method to retrieve allergies and nav to Profile Page and store in survey database
+    private void goToProfilePage() {
         allergiesData = allergiesEditText.getText().toString();
 
-        // nav to Home Page
-        Intent intent = new Intent(SurveyActivity.this, HomePage.class);
-        intent.putExtra("SKIN_TYPE", selectedSkinType); // pass the selected skin type
-        intent.putExtra("ALLERGIES", allergiesData);    // pass allergies data
+        // Convert boolean values to 1s and 0s
+        int oilyValue = isOily ? 1 : 0;
+        int dryValue = isDry ? 1 : 0;
+        int acneValue = isAcne ? 1 : 0;
+        int comboValue = isCombo ? 1 : 0;
+
+        // Store survey data in the survey.db database
+        SQLiteDatabase surveyDb = new SurveyDatabaseHelper(this).getWritableDatabase();
+
+        try {
+            ContentValues surveyValues = new ContentValues();
+            surveyValues.put(SurveyDatabaseHelper.COLUMN_OILY, oilyValue);
+            surveyValues.put(SurveyDatabaseHelper.COLUMN_DRY, dryValue);
+            surveyValues.put(SurveyDatabaseHelper.COLUMN_ACNE, acneValue);
+            surveyValues.put(SurveyDatabaseHelper.COLUMN_COMBO, comboValue);
+            surveyValues.put(SurveyDatabaseHelper.COLUMN_ALLERGIES, allergiesData);
+
+            long newRowId = surveyDb.insert(SurveyDatabaseHelper.TABLE_SURVEY, null, surveyValues);
+
+            if (newRowId == -1) {
+                Log.e("SurveyDatabaseError", "Error inserting survey data into the database");
+            }
+        } finally {
+            surveyDb.close();
+
+            // Display a Toast message indicating the successful creation of the survey database
+            Toast.makeText(SurveyActivity.this, "Survey database created successfully", Toast.LENGTH_SHORT).show();
+        }
+
+        // intent to nav to Profile Page
+        Intent intent = new Intent(SurveyActivity.this, ProfilePage.class);
+
+        // pass selected skin types to Profile Page
+        intent.putExtra("IS_OILY", isOily);
+        intent.putExtra("IS_DRY", isDry);
+        intent.putExtra("IS_ACNE", isAcne);
+        intent.putExtra("IS_COMBO", isCombo);
+        intent.putExtra("ALLERGIES", allergiesData);    // pass allergies data Profile Page
+
+        // start ProfilePage
         startActivity(intent);
-        finish();
+        finish();   // finish SurveyActivity
     }
+
+    private class LoadDataTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                loadCSVData();
+            } catch (IOException e) {
+                Log.e("LoadDataTask", "Error loading CSV data: " + e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(SurveyActivity.this, "Data loaded successfully", Toast.LENGTH_SHORT).show();
+            Log.d("LoadDataTask", "Data loaded successfully");
+        }
+    }
+
+    private void loadCSVData() throws IOException {
+        AssetManager assetManager = getAssets();
+        InputStream inputStream = assetManager.open("ingredients.csv");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        SQLiteDatabase db = new SkincareDatabaseHelper(this).getWritableDatabase();
+
+        try {
+            String line;
+            reader.readLine(); // Skip the header line
+            while ((line = reader.readLine()) != null) {
+                String[] rowData = line.split(",");
+                if (rowData.length == 5) {
+                    String ingredientName = rowData[0];
+                    int acne = Integer.parseInt(rowData[1]);
+                    int oily = Integer.parseInt(rowData[2]);
+                    int dry = Integer.parseInt(rowData[3]);
+                    int combo = Integer.parseInt(rowData[4]);
+
+                    ContentValues values = new ContentValues();
+                    values.put(SkincareDatabaseHelper.COLUMN_INGREDIENT_NAME, ingredientName);
+                    values.put(SkincareDatabaseHelper.COLUMN_ACNE, acne);
+                    values.put(SkincareDatabaseHelper.COLUMN_OILY, oily);
+                    values.put(SkincareDatabaseHelper.COLUMN_DRY, dry);
+                    values.put(SkincareDatabaseHelper.COLUMN_COMBO, combo);
+
+                    // Insert with conflict ignore to avoid updating existing data
+                    long newRowId = db.insertWithOnConflict(
+                            SkincareDatabaseHelper.TABLE_INGREDIENTS,
+                            null,
+                            values,
+                            SQLiteDatabase.CONFLICT_IGNORE
+                    );
+
+                    if (newRowId == -1) {
+                        Log.d("DatabaseInfo", "Data already exists: " + ingredientName);
+                    }
+                }
+            }
+        } finally {
+            reader.close();
+            db.close();
+        }
+    }
+
 }
--- app/src/main/java/com/cs407/skinsavvy/SurveyDatabaseHelper.java
+++ app/src/main/java/com/cs407/skinsavvy/SurveyDatabaseHelper.java
@@ -4,41 +4,41 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class SkincareDatabaseHelper extends SQLiteOpenHelper {
+// SurveyDatabaseHelper.java
+public class SurveyDatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "ingredients.db";
+    private static final String DATABASE_NAME = "survey.db";
     private static final int DATABASE_VERSION = 1;
-    public static final String TABLE_INGREDIENTS = "ingredients";
+
+    public static final String TABLE_SURVEY = "survey";
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_INGREDIENT_NAME = "ingredient_name";
-    public static final String COLUMN_ACNE = "acne";
     public static final String COLUMN_OILY = "oily";
     public static final String COLUMN_DRY = "dry";
+    public static final String COLUMN_ACNE = "acne";
     public static final String COLUMN_COMBO = "combo";
+    public static final String COLUMN_ALLERGIES = "allergies";
 
-    private static final String CREATE_TABLE_INGREDIENTS =
-            "CREATE TABLE " + TABLE_INGREDIENTS + " (" +
+    private static final String CREATE_TABLE_SURVEY =
+            "CREATE TABLE " + TABLE_SURVEY + " (" +
                     COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_INGREDIENT_NAME + " TEXT, " +
-                    COLUMN_ACNE + " INTEGER, " +
                     COLUMN_OILY + " INTEGER, " +
                     COLUMN_DRY + " INTEGER, " +
-                    COLUMN_COMBO + " INTEGER)";
+                    COLUMN_ACNE + " INTEGER, " +
+                    COLUMN_COMBO + " INTEGER, " +
+                    COLUMN_ALLERGIES + " TEXT)";
 
-    public SkincareDatabaseHelper(Context context) {
+    public SurveyDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_INGREDIENTS);
+        db.execSQL(CREATE_TABLE_SURVEY);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_INGREDIENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SURVEY);
         onCreate(db);
     }
 }
-
-
